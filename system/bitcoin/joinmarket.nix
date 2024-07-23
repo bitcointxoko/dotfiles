0e317a5a1b0d2967a6f03bf61b8ee3c01a09fc46
Bitcoin Txoko <142011724+bitcointxoko@users.noreply.github.com>
2024-07-23 09:29:02 +0200

    Add comments to joinmarket.nix

--- system/bitcoin/joinmarket.nix
+++ system/bitcoin/joinmarket.nix
@@ -1,5 +1,19 @@
+### JOINMARKET
 {
-  services.joinmarket = {
+  services = {
+    joinmarket = {
+    # Set this to enable the JoinMarket service, including its command-line scripts.
+    # These scripts have prefix 'jm-', like 'jm-tumbler'.
+    # Note: JoinMarket has full access to bitcoind, including its wallet functionality.
     enable = true;
-  };
+    # 
+    # Set this to enable the JoinMarket Yield Generator Bot. You will be able to
+    # earn sats by providing CoinJoin liquidity. This makes it impossible to use other
+    # scripts that access your wallet.
+    # services.joinmarket.yieldgenerator.enable = true;
+    #
+    };
+    # Set this to enable the JoinMarket order book watcher.
+    joinmarket-ob-watcher.enable = true;
+  }; 
 }
