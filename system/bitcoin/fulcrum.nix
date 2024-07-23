69a259ab81b1c6cb7f4e023a4577164138d8c8f2
Bitcoin Txoko <142011724+bitcointxoko@users.noreply.github.com>
2024-07-23 09:20:27 +0200

    Add comments to fulcrum.nix

--- system/bitcoin/fulcrum.nix
+++ system/bitcoin/fulcrum.nix
@@ -1,5 +1,14 @@
+### FULCRUM
 {
   services.fulcrum = {
+    # Set this to enable fulcrum, an Electrum server implemented in C++.
+    #
+    # Compared to electrs, fulcrum has higher storage demands but
+    # can serve arbitrary address queries instantly.
+    #
+    # Before enabling fulcrum, and for more info on storage demands,
+    # see the description of option `enable` in ../modules/fulcrum.nix
+    #
     enable = true;
   };
 }
