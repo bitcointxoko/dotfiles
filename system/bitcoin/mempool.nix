41eb2f2d7e7c9c0947c0d6b86dce3437589e5776
Bitcoin Txoko <142011724+bitcointxoko@users.noreply.github.com>
2024-07-23 09:18:53 +0200

    Add comments to mempool.nix

--- system/bitcoin/mempool.nix
+++ system/bitcoin/mempool.nix
@@ -1,6 +1,18 @@
 {...}: {
+  ### MEMPOOL
   services.mempool = {
+    # Set this to enable mempool, a fully featured Bitcoin visualizer, explorer,
+    # and API service.
     enable = true;
+
+    # Possible options for the Electrum backend server:
+    #
+    # - electrs (enabled by default):
+    #   Small database size, slow when querying new addresses.
+    #
+    # - fulcrum:
+    #   Large database size, quickly serves arbitrary address queries.
+    #
     electrumServer = "fulcrum";
     frontend.enable = true;
   };
