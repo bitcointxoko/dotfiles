50f2aed02d6ebc7009ae01ce5a24ace6a4ba7266
Bitcoin Txoko <142011724+bitcointxoko@users.noreply.github.com>
2024-07-23 10:25:04 +0200

    Create liquidd.nix

---
+++ system/bitcoin/liquidd.nix
@@ -0,0 +1,7 @@
+  ### LIQUIDD
+  # Enable this module to use Liquid, a sidechain for an inter-exchange
+  # settlement network linking together cryptocurrency exchanges and
+  # institutions around the world.
+  { services.liquidd.enable = true; }
+  #
+  # Liquid can be controlled with command 'elements-cli'.
