59bfba218bc877c649c06dae136cbb457ba045b5
Bitcoin Txoko <142011724+bitcointxoko@users.noreply.github.com>
2024-07-23 09:10:08 +0200

    Add comments to clightning.nix

--- system/bitcoin/clightning.nix
+++ system/bitcoin/clightning.nix
@@ -1,10 +1,34 @@
-{...}: {
+{
+  ### CLIGHTNING
+  # Enable clightning, a Lightning Network implementation in C.
   services = {
     clightning = {
       enable = true;
       extraConfig = ''
         experimental-offers
       '';
+      # == Plugins
+      # See ../README.md (Features → clightning) for the list of available plugins.
+      # plugins.clboss.enable = true;
     };
+    # == REST server
+    # Set this to create a clightning REST onion service.
+    # This also adds binary `lndconnect-clightning` to the system environment.
+    # This binary creates QR codes or URLs for connecting applications to clightning
+    # via the REST onion service.
+    # You can also connect via WireGuard instead of Tor.
+    # See ../docs/services.md for details.
+    #
+    # clightning-rest = {
+    #   enable = true;
+    #   lndconnect = {
+    #     enable = true;
+    #     onion = true;
+    #   };
+    # };
   };
+  # Set this to create an onion service by which clightning can accept incoming connections
+  # via Tor.
+  # The onion service is automatically announced to peers.
+  # nix-bitcoin.onionServices.clightning.public = true;
 }
