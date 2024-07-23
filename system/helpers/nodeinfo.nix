8c87e74bb9a91ba7655e9b01ba44968079eb6ea2
Bitcoin Txoko <142011724+bitcointxoko@users.noreply.github.com>
2024-07-23 10:50:18 +0200

    Create nodeinfo.nix

---
+++ system/helpers/nodeinfo.nix
@@ -0,0 +1,5 @@
+### Nodeinfo
+# Set this to add command `nodeinfo` to the system environment.
+# It shows info about running services like onion addresses and local addresses.
+# It is enabled by default when importing `secure-node.nix`.
+{ nix-bitcoin.nodeinfo.enable = true; }
