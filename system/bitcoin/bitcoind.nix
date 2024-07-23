be84d63fc47f2de0c64658cc5f2b6225ec0f2752
Bitcoin Txoko <142011724+bitcointxoko@users.noreply.github.com>
2024-07-23 09:04:11 +0200

    Add comments to bitcoind.nix

--- system/bitcoin/bitcoind.nix
+++ system/bitcoin/bitcoind.nix
@@ -1,6 +1,12 @@
 {config, ...}: {
+  ### BITCOIND
+  # Bitcoind is enabled by default via secure-node.nix.
   services.bitcoind = {
     enable = true;
+    # Set this option to enable pruning with a specified MiB value.
+    # clightning is compatible with pruning. See
+    # https://github.com/ElementsProject/lightning/#pruning for more information.
+    # LND and electrs are not compatible with pruning.
     prune = 0;
     txindex = true;
     zmqpubrawtx = "tcp://127.0.0.1:28333";
@@ -15,10 +21,16 @@
         "0.0.0.0/0" # Allow on all addresses
       ];
     };
+    # You can add options that are not defined in modules/bitcoind.nix as follows
+    # extraConfig = ''
+    #   maxorphantx=110
+    # '';
   }; 
   networking.firewall.allowedTCPPorts = [
     config.services.bitcoind.port
     config.services.bitcoind.rpc.port
   ];
-  nix-bitcoin.onionServices.bitcoind.public = true; 
+  # Set this to accounce the onion service address to peers.
+  # The onion service allows accepting incoming connections via Tor.
+  # nix-bitcoin.onionServices.bitcoind.public = true;
 }
