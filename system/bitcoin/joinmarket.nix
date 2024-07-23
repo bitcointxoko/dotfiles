22a0994b3dbc6f499693928f7167914c535026a9
Bitcoin Txoko <142011724+bitcointxoko@users.noreply.github.com>
2024-07-23 09:29:48 +0200

    Update joinmarket.nix

--- system/bitcoin/joinmarket.nix
+++ system/bitcoin/joinmarket.nix
@@ -10,7 +10,7 @@
     # Set this to enable the JoinMarket Yield Generator Bot. You will be able to
     # earn sats by providing CoinJoin liquidity. This makes it impossible to use other
     # scripts that access your wallet.
-    # services.joinmarket.yieldgenerator.enable = true;
+    # yieldgenerator.enable = true;
     };
     # Set this to enable the JoinMarket order book watcher.
     joinmarket-ob-watcher.enable = true;
