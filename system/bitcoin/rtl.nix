bc5eac966e8e86853a45f72d1ed55ae5bf141ca9
Bitcoin Txoko <142011724+bitcointxoko@users.noreply.github.com>
2024-07-23 09:16:26 +0200

    Add comments to rtl.nix

--- system/bitcoin/rtl.nix
+++ system/bitcoin/rtl.nix
@@ -1,9 +1,21 @@
 {
+  ### RIDE THE LIGHTNING
   services.rtl = {
+    # Set this to enable RTL, a web interface for lnd and clightning.
     enable = true;
     nodes = {
+      # Set this to add a lnd node interface.
+      # Automatically enables lnd.
       # lnd.enable = true;
-      clightning.enable = true;
+      # Set this to add a clightning node interface.
+      # Automatically enables clightning.
+      # clightning.enable = true;
+      #
+      # You can enable both nodes simultaneously.
+      #
+      # Set this option to enable swaps with lightning-loop.
+      # Automatically enables lightning-loop.
+      # lnd.loop = true;
     };
   };
 }
